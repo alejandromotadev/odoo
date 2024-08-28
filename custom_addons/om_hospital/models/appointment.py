ff379abc3600a57ee606f2992e2db95d0a1e2f00
alejandro mota <aamotadev@gmail.com>
2024-08-28 17:52:24 -0600

    Update appointment.py

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -4,6 +4,7 @@ class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _inherit = ['mail.thread'] # Nos permite trackear las acciones que se apliquen en este modulo/modelo
     _description = 'Hospital Appointment'
+    _rec_name = 'patient_id' # Recupera el valor name del modelo hospital.patient (patient_id viene en el Many2one)
 
     patient_id = fields.Many2one('hospital.patient', string='Patient', required=True, tracking=True)
     date_appointment = fields.Datetime(string='Fecha de la cita', required=True, tracking=True)
